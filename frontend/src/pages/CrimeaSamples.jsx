e0404634a399dd976b77c5f7e833daf41c06fded
itdesminic <v-hector.mairena@equinoxgold.com>
2026-07-19 11:45:34 -0600

    fix: import useEffect from react in CrimeaSamples.jsx

--- frontend/src/pages/CrimeaSamples.jsx
+++ frontend/src/pages/CrimeaSamples.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import api, { getBackendUrl } from '../services/api'
 import { Link } from 'react-router-dom'
 import SignatureCanvas from 'react-signature-canvas'
